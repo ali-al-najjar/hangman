77a8374dcd365d20b140f1e16a02ac0db4526f4c
aliynajjar <ali.y.alnajjar@gmail.com>
2023-02-28 03:23:58 +0200

    Added random word function

--- js/script.js
+++ js/script.js
@@ -3,17 +3,22 @@ const categories = document.getElementById('categories');
 const letters = document.getElementById("letters");
 const hanging_man = document.getElementById("hanging_man");
 const new_game = document.getElementById("new_game");
+const header = document.getElementById('header');
+const alphabet = ["a","b","c","d","e","f","g","h","i","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const user_input = document.getElementById('user_input');
 let tries = 0;
 let count = 0;
-let alphabet = ["a","b","c","d","e","f","g","h","i","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-let created_word ="";
-let header = document.getElementById('header');
+
+let word ="";
+
 let flag = false;
 
-let categories_dictionary = {
+const categories_dictionary = {
   cities:["Beirut","Baabda","Baalbeck","Saida","Sour"],
-  cars:["Toyota","Mercedes","Honda","Nissan","Ferrari"]
-}
+  cars:["Toyota","Mercedes","Honda","Nissan","Ferrari"],
+};
+
+console.log(categories_dictionary);
 
 const categoriesButtons = () => {
   header.innerHTML = `<div>Choose from the below categories</div>`;
@@ -22,6 +27,7 @@ const categoriesButtons = () => {
     category_button.innerHTML += `<button class="category" onclick ="createWord('${key}')">${key} </button>`;
   }
   categories_container.appendChild(category_button);
+
 };
 
 const createWord = (category) =>{
@@ -32,11 +38,21 @@ const createWord = (category) =>{
       flag= false;
     }else{
     button.disabled = true;
-    }
-
+    }  
   });
- 
-}
+
+  letters.classList.remove('hide');
+  user_input.innerText="";
+
+  let words = categories_dictionary[category];
+  word = words[Math.floor(Math.random() * words.length)];
+  word = word.toUpperCase();
+  console.log(word);
+
+  
+};
+
+
 const alphabetLetter = () => {
   for (let i=0; i<alphabet.length;i++){
     letters.innerHTML += `<button class = letter>${alphabet[i]}</button`;
@@ -50,6 +66,7 @@ const start_game = () => {
   count = 0;
   categoriesButtons();
   alphabetLetter();
+  
 };
 
 new_game.addEventListener("click",start_game);
